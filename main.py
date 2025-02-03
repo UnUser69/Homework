079829ec10fd91a5089333ea2bd6193f49f2f751
UnUser69 <111284062+UnUser69@users.noreply.github.com>
2025-02-03 20:24:54 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+#1st program
+print((9*5)**0.5)
+
+#2nd program
+print(9.99>9.98 and 1000!=1000.1)
+
+#3rd program
+a=2*2+2
+b=2*(2+2)
+print(a)
+print(b)
+print(a>b)
+
+#4rd program
+a = str(123.456)
+print(a[4])
+
+#alternative result
+a = str(123.456)
+a = float(a)
+a = a*10
+a = int(a)
+a = a%10
+print(a)
